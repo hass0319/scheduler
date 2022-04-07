62f6ca2ea2397f3374dfbfd9d4c6394a24ae2243
Amro <amro.abdrabou@hotmail.co.uk>
2022-04-07 02:48:40 -0400

    fixes failing test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent
+} from "@testing-library/react";
 
-import Application from "components/Application.js";
+import Application from "components/Application";
 
 afterEach(cleanup);
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
 
-it("renders without crashing", () => {
-  render(<Application />);
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
